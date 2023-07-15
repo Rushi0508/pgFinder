4064b55dd12aaa836060c45dd3e0202a0d9d7ab3
Nishant5789 <nishantbhandari218@gmail.com>
2023-07-15 18:23:24 +0530

    feat : added jsx for  finderdashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import CreateProperty from './components/CreateProperty';
+import { FinderDashboard } from './components/FinderDashboard';
 import LandingPage from './components/LandingPage';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
@@ -14,6 +15,7 @@ function App() {
                 <Route  path="/login" element={<Login />} />
                 <Route  path="/register" element={<Register />} />
                 <Route  path="/pg/create" element={<CreateProperty />} />
+                <Route  path="/pg/all" element={<FinderDashboard />} />
             </Routes>
         </>
     )
